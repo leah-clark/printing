cfa4f3ae113ac60d90e200d50190c03550634f5c
lclar <leah.a.clark1@aexp.com>
2018-06-21 12:35:20 +0100

    error handling

--- src/main/java/com/exercise/printing/PrintingController.java
+++ src/main/java/com/exercise/printing/PrintingController.java
@@ -30,17 +30,31 @@ public class PrintingController {
     @RequestMapping(value="/printing/{variance}/{image}", method= GET)
     public PrinterResponse printing(@RequestParam(value="image", defaultValue="Invalid") String image,
                                     @RequestParam(value = "variance", defaultValue = "3") String variance) throws IOException {
-        String colourRGB = imageToColour(image, Integer.parseInt(variance));
-        return new PrinterResponse(colourRGB,
-                String.format(image));
+        try {
+            String colourRGB = imageToColour(image, Integer.parseInt(variance));
+
+            return new PrinterResponse(colourRGB,
+                    String.format(image));
+        }
+        catch (IOException e) {
+            e.getMessage();
+            return new PrinterResponse("No Colour",
+                    "No URL - there has been an error");
+        }
     }
 
     public String imageToColour(final String imageUrl, int variance) throws IOException {
         final URL url = new URL(imageUrl);
         BufferedImage img = ImageIO.read(url);
         String colourRGB = colourConverter.getColour(img, variance);
-        ColourDetails colourDetails = getColourFromJson("http://thecolorapi.com/id?"+colourRGB);
-        return colourDetails.getName().getValue();
+        try {
+            ColourDetails colourDetails = getColourFromJson("http://thecolorapi.com/id?" + colourRGB);
+            return colourDetails.getName().getValue();
+        }
+        catch (IOException e) {
+            e.getMessage();
+            return "No match with any colour from the api";
+        }
     }
 
     public ColourDetails getColourFromJson(String url) throws IOException {
